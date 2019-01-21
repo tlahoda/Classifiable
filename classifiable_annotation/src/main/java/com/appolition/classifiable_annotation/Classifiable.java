102dbf99970c1d38cfaf241c0ec7c2fb46462dc1
Thomas Lahoda <tlahoda@gmail.com>
2019-01-21 17:14:03 -0500

    added java docs

--- classifiable_annotation/src/main/java/com/appolition/classifiable_annotation/Classifiable.java
+++ classifiable_annotation/src/main/java/com/appolition/classifiable_annotation/Classifiable.java
@@ -5,6 +5,9 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Annotates methods for which to generate an enum
+ */
 @Target({ElementType.METHOD})
 @Retention(RetentionPolicy.SOURCE)
 public @interface Classifiable {
