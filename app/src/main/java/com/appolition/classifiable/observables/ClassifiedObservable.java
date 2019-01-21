d6c7df8a87ce0e4be0e5cfa7fe84f0ee7f6255d3
Thomas Lahoda <tlahoda@gmail.com>
2019-01-20 23:30:30 -0500

    made parcelable

--- app/src/main/java/com/appolition/classifiable/observables/ClassifiedObservable.java
+++ app/src/main/java/com/appolition/classifiable/observables/ClassifiedObservable.java
@@ -1,48 +1,110 @@
 package com.appolition.classifiable.observables;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import androidx.annotation.NonNull;
-import androidx.databinding.Observable;
-import androidx.databinding.PropertyChangeRegistry;
 
-//Based off of BaseObservable
-public class ClassifiedObservable implements Observable {
-    private transient PropertyChangeRegistry callbacks;
+import org.parceler.Parcel;
 
-    public ClassifiedObservable() {
-        callbacks = new PropertyChangeRegistry();
-    }
+//Based off of BaseObservable from Android databinding
+@Parcel
+public class ClassifiedObservable<EnumType extends Enum<EnumType>> {
+    private transient Map<EnumType, List<OnPropertChangedCallback<EnumType>>> callbacks;
 
-    @Override
-    public void addOnPropertyChangedCallback(@NonNull OnPropertyChangedCallback callback) {
+    public ClassifiedObservable<EnumType> add(EnumType enumType, @NonNull OnPropertChangedCallback<EnumType> callback) {
         synchronized (this) {
             if (callbacks == null) {
-                callbacks = new PropertyChangeRegistry();
+                callbacks = new HashMap<>();
             }
         }
 
-        callbacks.add(callback);
-    }
+        List<OnPropertChangedCallback<EnumType>> enumCallbacks = callbacks.get(enumType);
 
-    @Override
-    public void removeOnPropertyChangedCallback(@NonNull OnPropertyChangedCallback callback) {
-        synchronized (this) {
-            if (callbacks == null) {
-                return;
-            }
+        if (enumCallbacks == null) {
+            enumCallbacks = new ArrayList<>();
+
+            callbacks.put(enumType, enumCallbacks);
         }
 
-        callbacks.remove(callback);
+        enumCallbacks.add(callback);
+
+        return this;
     }
 
-    public ClassifiedObservable notifyPropertyChanged(int propertyId) {
+    public ClassifiedObservable<EnumType> remove(EnumType enumType, @NonNull OnPropertChangedCallback<EnumType> callback) {
         synchronized (this) {
             if (callbacks == null) {
                 return this;
             }
         }
 
-        callbacks.notifyChange(this, propertyId);
+        List<OnPropertChangedCallback<EnumType>> enumCallbacks = callbacks.get(enumType);
+
+        if (enumCallbacks == null) {
+            return this;
+        }
+
+        enumCallbacks.remove(callback);
+
+        if (enumCallbacks.size() == 0) {
+            callbacks.remove(enumType);
+        }
 
         return this;
+    }
+
+    public ClassifiedObservable<EnumType> clear() {
+        synchronized (this) {
+            if (callbacks == null) {
+                return this;
+            }
+        }
+
+        callbacks.clear();
+
+        return this;
+    }
+
+    public ClassifiedObservable<EnumType> clear(EnumType enumType) {
+        synchronized (this) {
+            if (!callbacks.containsKey(enumType)) {
+                return this;
+            }
+        }
+
+        callbacks.remove(enumType);
+
+        return this;
+    }
+
+    public ClassifiedObservable<EnumType> notifyPropertyChanged(EnumType enumType) {
+        synchronized (this) {
+            if (callbacks == null) {
+                return this;
+            }
+        }
+
+        List<OnPropertChangedCallback<EnumType>> enumCallbacks = callbacks.get(enumType);
+
+        if (enumCallbacks == null || enumCallbacks.size() == 0) {
+            return this;
+        }
+
+        for (OnPropertChangedCallback<EnumType> callback : enumCallbacks) {
+            callback.notitfyChanged(enumType);
+        }
+
+        return this;
+    }
+
+    //This is an static class instead of an interface for Parceler
+    @Parcel
+    public static class OnPropertChangedCallback<EnumType extends Enum<EnumType>> {
+        public void notitfyChanged(EnumType enumType) {
+            //noop
+        }
     }
 }
